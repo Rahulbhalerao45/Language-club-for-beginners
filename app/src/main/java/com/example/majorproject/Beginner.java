e0f77f5c1c38c94cf127a9fb690dc8a86ab4a892
Rohit Yadav <ry5322014@gmail.com>
2023-03-29 16:35:59 +0530

    Initial Commit

--- app/src/main/java/com/example/majorproject/Beginner.java
+++ app/src/main/java/com/example/majorproject/Beginner.java
@@ -7,47 +7,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
-public class Quiz extends AppCompatActivity {
-
+public class Beginner extends AppCompatActivity {
 
     EditText mainUsername;
-    private Button mReturnButton, mbeginnerButton;
+    TextView question1, question2;
+    Button mReturnButton;
+    RadioButton answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8;
     String username;
-
     TextView logoutRedirectText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
+        setContentView(R.layout.activity_beginner);
 
-        // Retrieve the username passed from LoginActivity
         username = getIntent().getStringExtra("USERNAME");
-        mainUsername = findViewById(R.id.main_username4);
 
-        mbeginnerButton = findViewById(R.id.beginner);
-        mReturnButton = findViewById(R.id.return_button);
-        logoutRedirectText = findViewById(R.id.logout4);
+        mainUsername = findViewById(R.id.beginner1_main_username);
 
         mainUsername.setText(username);
         mainUsername.setEnabled(false); // disable editing of the username field
 
-        mbeginnerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(Quiz.this, Beginner.class);
-                intent.putExtra("USERNAME", username);
-                startActivity(intent);
-            }
-        });
+        question1 = findViewById(R.id.beginner1_question1);
+        question2 = findViewById(R.id.beginner1_question2);
+        answer1 = findViewById(R.id.beginner1_answer1);
+        answer2 = findViewById(R.id.beginner1_answer2);
+        answer5 = findViewById(R.id.beginner1_answer5);
+        answer6 = findViewById(R.id.beginner1_answer6);
+        mReturnButton = findViewById(R.id.beginner1_return_button);
 
         mReturnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Quiz.this, Learning.class);
+                Intent intent = new Intent(Beginner.this, Quiz.class);
                 intent.putExtra("USERNAME", username);
                 startActivity(intent);
             }
@@ -56,10 +51,9 @@ public class Quiz extends AppCompatActivity {
         logoutRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent =new Intent(Quiz.this, LoginActivity.class);
+                Intent intent =new Intent(Beginner.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
-
     }
 }
