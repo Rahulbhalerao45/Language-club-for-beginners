29b617074dc645f24a8a78da4f6fa16a3bbd2cff
Rohit Yadav <ry5322014@gmail.com>
2023-04-06 12:44:17 +0530

    Initial Commit

--- app/src/main/java/com/example/majorproject/Quiz.java
+++ app/src/main/java/com/example/majorproject/Quiz.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,158 +21,48 @@ import com.google.firebase.database.ValueEventListener;
 
 public class Quiz extends AppCompatActivity {
 
+
     EditText mainUsername;
-    Button return1Button, beginnerButton, mediumButton, advancedButton;
+    Button returnButton;
+    RadioButton beginnerOption1A, beginnerOption1B, beginnerOption2A, beginnerOption2B, beginnerOption3A, beginnerOption3B;
 
-    String username, language, question1, option1A, option1B, question2, option2A, option2B, question3, option3A, option3B;
-    TextView logoutRedirectText;
+    RadioGroup group1, group2, group3;
 
-    DatabaseReference quizRef, ref;// reference to the quiz data in Firebase
+    String username, language;
+    TextView logoutRedirectText, beginnerQuestion1, beginnerQuestion2, beginnerQuestion3;
+
+    DatabaseReference ref;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        // Retrieve the username and language passed from LoginActivity
-        username = getIntent().getStringExtra("USERNAME");
-        language = getIntent().getStringExtra("LANGUAGE");
+        // Retrieve the username passed from LoginActivity
+        String username = getIntent().getStringExtra("USERNAME");
+        String language = getIntent().getStringExtra("LANGUAGE");
 
+        beginnerQuestion1 = findViewById(R.id.quiz_question1);
+        beginnerQuestion2 = findViewById(R.id.quiz_question2);
+        beginnerQuestion3 = findViewById(R.id.quiz_question3);
+        group1 = findViewById(R.id.qradio1);
+        group2 = findViewById(R.id.qradio2);
+        group3 = findViewById(R.id.qradio3);
+        beginnerOption1A = findViewById(R.id.qanswer1);
+        beginnerOption1B = findViewById(R.id.qanswer2);
+        beginnerOption2A = findViewById(R.id.qanswer3);
+        beginnerOption2B = findViewById(R.id.qanswer4);
+        beginnerOption3A = findViewById(R.id.qanswer5);
+        beginnerOption3B = findViewById(R.id.qanswer6);
         mainUsername = findViewById(R.id.quiz_main_username);
-        return1Button = findViewById(R.id.quiz_return_button);
-        beginnerButton = findViewById(R.id.quiz_beginner_button);
-        mediumButton = findViewById(R.id.quiz_medium_button);
-        advancedButton = findViewById(R.id.quiz_advanced_button);
+        returnButton = findViewById(R.id.quiz_return_button);
         logoutRedirectText = findViewById(R.id.quiz_logout);
 
         mainUsername.setText("" + username + "--" + language + "");
         mainUsername.setEnabled(false); // disable editing of the username field
 
-        // Get a reference to the quiz data in Firebase using the language key
-       DatabaseReference quizRef = FirebaseDatabase.getInstance().getReference().child("quiz").child(language);
-
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("quiz/language/beginner");
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    // Retrieve the questions and options
-                    String question1 = snapshot.child("question1/question").getValue(String.class);
-                    String option1A = snapshot.child("question1/options/A").getValue(String.class);
-                    String option1B = snapshot.child("question1/options/B").getValue(String.class);
-
-                    String question2 = snapshot.child("question2/question").getValue(String.class);
-                    String option2A = snapshot.child("question2/options/A").getValue(String.class);
-                    String option2B = snapshot.child("question2/options/B").getValue(String.class);
-
-                    String question3 = snapshot.child("question3/question").getValue(String.class);
-                    String option3A = snapshot.child("question3/options/A").getValue(String.class);
-                    String option3B = snapshot.child("question3/options/B").getValue(String.class);
-
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                // Handle the error
-            }
-        });
-        beginnerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Select the beginner level key from the quiz data using a switch statement
-                quizRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        String quizLevelKey = "";
-                        switch (v.getId()) {
-                            case R.id.quiz_beginner_button:
-                                quizLevelKey = "beginner";
-                                break;
-                        }
-                        Intent intent = new Intent(Quiz.this, Beginner.class);
-                        intent.putExtra("USERNAME", username);
-                        intent.putExtra("LANGUAGE", language);
-                        intent.putExtra("LEVEL", quizLevelKey);
-                        intent.putExtra("QUESTION1", question1);
-                        intent.putExtra("QUESTION2", question2);
-                        intent.putExtra("QUESTION3", question3);
-                        intent.putExtra("OPTION1A", option1A);
-                        intent.putExtra("OPTION1B", option1B);
-                        intent.putExtra("OPTION2A", option2A);
-                        intent.putExtra("OPTION2B", option2B);
-                        intent.putExtra("OPTION3A", option3A);
-                        intent.putExtra("OPTION3B", option3B);
-                        startActivity(intent);
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                        Toast.makeText(Quiz.this, "Error accessing quiz data", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-
-        mediumButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Select the beginner level key from the quiz data using a switch statement
-                quizRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        String quizLevelKey = "";
-                        switch (v.getId()) {
-                            case R.id.quiz_medium_button:
-                                quizLevelKey = "medium";
-                                break;
-                        }
-
-                        Intent intent = new Intent(Quiz.this, Medium.class);
-                        intent.putExtra("USERNAME", username);
-                        intent.putExtra("LANGUAGE", language);
-                        intent.putExtra("LEVEL", quizLevelKey);
-                        startActivity(intent);
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                        Toast.makeText(Quiz.this, "Error accessing quiz data", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-
-        advancedButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Select the beginner level key from the quiz data using a switch statement
-                quizRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        String quizLevelKey = "";
-                        switch (v.getId()) {
-                            case R.id.quiz_advanced_button:
-                                quizLevelKey = "advanced";
-                                break;
-                        }
-
-                        Intent intent = new Intent(Quiz.this, Advanced.class);
-                        intent.putExtra("USERNAME", username);
-                        intent.putExtra("LANGUAGE", language);
-                        intent.putExtra("LEVEL", quizLevelKey);
-                        startActivity(intent);
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                        Toast.makeText(Quiz.this, "Error accessing quiz data", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-
-        return1Button.setOnClickListener(new View.OnClickListener() {
+        returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Quiz.this, Learning.class);
