a90cef4ccdd6960ebb78d4e060ad5aae1d7b05e6
Rohit Yadav <ry5322014@gmail.com>
2023-04-07 20:05:43 +0530

    Initial Commit

--- app/src/main/java/com/example/majorproject/Quiz.java
+++ app/src/main/java/com/example/majorproject/Quiz.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,6 +35,9 @@ public class Quiz extends AppCompatActivity {
     DatabaseReference quiz1Ref, quiz2Ref, quiz3Ref;// reference to the quiz data in Firebase
 
     FirebaseDatabase Reference;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
 
     @Override
