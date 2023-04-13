533badb337c7f65cada83f89fa6500ce8044f43e
Rohit Yadav <ry5322014@gmail.com>
2023-04-13 09:52:29 +0530

    Initial Commit

--- app/src/main/java/com/example/majorproject/Profile.java
+++ app/src/main/java/com/example/majorproject/Profile.java
@@ -1,9 +1,13 @@
 package com.example.majorproject;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,17 +15,24 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Profile extends AppCompatActivity {
 
-    EditText mainUsername, ranking, search;
+    EditText mainUsername, ranking;
+
+    Spinner search;
 
     Button return2Button, logoutButton, updateButton;
 
@@ -36,8 +47,6 @@ public class Profile extends AppCompatActivity {
     DatabaseReference databaseReference;
 
     TextView logoutRedirectText;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,8 +76,6 @@ public class Profile extends AppCompatActivity {
                 selectedLanguage4 = adapterView.getItemAtPosition(i).toString();
             }
         });
-
-
 
         return2Button.setOnClickListener(new View.OnClickListener() {
             @Override
